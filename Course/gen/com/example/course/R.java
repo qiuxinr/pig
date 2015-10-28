a51fd5607f381b2ed85aa2332183c9ac9765fa4e
qiuxinrong <944248764@qq.com>
2015-10-28 21:57:14 +0800

    完善细节
    完善细节

--- Course/gen/com/example/course/R.java
+++ Course/gen/com/example/course/R.java
@@ -200,26 +200,24 @@ containing a value of this type.
         public static final int welcome=0x7f020061;
     }
     public static final class id {
-        public static final int action_settings=0x7f040013;
+        public static final int action_settings=0x7f040011;
         public static final int adjust_content=0x7f040003;
-        public static final int cb1=0x7f04000f;
-        public static final int cb2=0x7f040010;
+        public static final int cb1=0x7f04000d;
+        public static final int cb2=0x7f04000e;
         public static final int de_login_logo=0x7f040005;
         public static final int elasticity_adjust=0x7f040004;
         public static final int horizontal=0x7f040000;
-        public static final int login_password=0x7f04000e;
-        public static final int login_username=0x7f04000d;
+        public static final int login_password=0x7f04000c;
+        public static final int login_username=0x7f04000b;
         public static final int not_adjust=0x7f040002;
-        public static final int rc_search_btn=0x7f04000b;
-        public static final int rc_search_ed=0x7f04000a;
         public static final int register_passwd=0x7f040007;
         public static final int register_submit=0x7f040009;
         public static final int register_username=0x7f040006;
         public static final int reregister_passwd=0x7f040008;
-        public static final int user_login_button=0x7f040011;
-        public static final int user_register_button=0x7f040012;
+        public static final int user_login_button=0x7f04000f;
+        public static final int user_register_button=0x7f040010;
         public static final int vertical=0x7f040001;
-        public static final int word_web_view=0x7f04000c;
+        public static final int word_web_view=0x7f04000a;
     }
     public static final class layout {
         public static final int activity_index=0x7f030000;
--- Course/src/com/example/course/LoginActivity.java
+++ Course/src/com/example/course/LoginActivity.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
@@ -19,8 +21,18 @@ public class LoginActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_seach);
-		buttontest=(Button)findViewById(R.id.rc_search_btn);
-		init();
+		//buttontest=(Button)findViewById(R.id.rc_search_btn);
+		 webView = (WebView) findViewById(R.id.word_web_view);
+	     
+		
+	     webView.getSettings().setJavaScriptEnabled(true);
+	     webView.setWebChromeClient(new WebChromeClient());
+	        //覆盖WebView默认使用第三方或系统默认浏览器打开网页的行为，使网页用WebView打开
+	         webView.loadUrl("http://m.kuaidi100.com"); 
+	         webView.setWebViewClient(new WebViewClient() );
+	    
+	      
+	       
 	}
 	
 /*	buttontest.setOnClickListener(new OnClickListener(){
@@ -42,23 +54,19 @@ public class LoginActivity extends Activity {
 	private void init(){
         webView = (WebView) findViewById(R.id.word_web_view);
         //WebView加载web资源
-       webView.loadUrl("http://baidu.com");
+       webView.loadUrl("http://http://m.kuaidi100.com/all/");
         //覆盖WebView默认使用第三方或系统默认浏览器打开网页的行为，使网页用WebView打开
        webView.setWebViewClient(new WebViewClient(){
-           @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            // TODO Auto-generated method stub
-               //返回值是true的时候控制去WebView打开，为false调用系统浏览器或第三方浏览器
-             view.loadUrl(url);
-            return true;
-        }
-       });
+    	   public boolean shouldOverrideUrlLoading(WebView view, String url) {
+               // TODO Auto-generated method stub
+                  //返回值是true的时候控制去WebView打开，为false调用系统浏览器或第三方浏览器
+                view.loadUrl(url);
+               return true;
+           }
+       }
+       );
     }
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.login, menu);
-		return true;
-	}
+	
+	
 
 }
